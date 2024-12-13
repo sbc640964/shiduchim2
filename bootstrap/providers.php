e3f79957e15997cffce55af411e5ba465046af37
abc640964 <sbc640964@gmail.com>
2024-12-13 09:03:03 +0200

    access to telescope dashboard

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\FamiliesPanelProvider::class,
     \App\Providers\HebcalServiceProvider::class,
+    \App\Providers\TelescopeServiceProvider::class,
 ];
