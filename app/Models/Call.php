98f9b501f01daec9129869f2035177cd7541009d
abc640964 <sbc640964@gmail.com>
2024-12-27 10:08:17 +0200

    Update Call model to include detailed family relations
    Enhanced the diary proposals mapping to display full family relations, including parents and maternal grandfather. This improves the clarity of relationship details in the call descriptions.

--- app/Models/Call.php
+++ app/Models/Call.php
@@ -252,7 +252,9 @@ HTML
                                 "השדכן: $this->user->name",
                                 "ההורה: ".$this->phoneModel->model->full_name,
                                 "ההצעות אליהם נידון בשיחה: ".$this->diaries->map(function (Diary $diary) {
-                                    return $diary->proposal->people->map->full_name->join(' עם ');
+                                    return $diary->proposal->people->map(function (Person $person) {
+                                        return $person->full_name .' בן של ' . $person->father->full_name . 'ו ' . $person->mother->full_name . " (בת של {$person->mother->father->full_name}) ";
+                                    })->join(' עם');
                                 })->join(' ו')
                             ]
                             ,' '),
