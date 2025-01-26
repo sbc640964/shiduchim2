bfcfbd256fd6b3cc1a20779e834ee9bf4673f384
abc640964 <sbc640964@gmail.com>
2025-01-26 09:58:05 +0200

    add WithoutOverlapping to TranscriptionCallJob

--- app/Jobs/TranscriptionCallJob.php
+++ app/Jobs/TranscriptionCallJob.php
@@ -3,16 +3,11 @@
 namespace App\Jobs;
 
 use App\Models\Call;
-use App\Models\Diary;
-use App\Models\Person;
-use Derrickob\GeminiApi\Data\Content;
-use Derrickob\GeminiApi\Data\GenerationConfig;
-use Derrickob\GeminiApi\Data\Schema;
-use Derrickob\GeminiApi\Gemini;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 
 class TranscriptionCallJob implements ShouldQueue
@@ -26,5 +21,12 @@ class TranscriptionCallJob implements ShouldQueue
     public function handle(): void
     {
         $this->call->refreshCallText();
+    }
+
+    public function middleware(): array
+    {
+        return [
+            new WithoutOverlapping(),
+        ];
     }
 }
