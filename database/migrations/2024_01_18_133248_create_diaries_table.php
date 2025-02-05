034b20b0fc3aa4ef63fca70d326bfa57e88ee798
abc640964 <sbc640964@gmail.com>
2025-02-05 16:33:15 +0200

    fix order in migrations

--- database/migrations/2024_01_18_133248_create_diaries_table.php
+++ database/migrations/2024_01_18_133248_create_diaries_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::disableForeignKeyConstraints();
+        Schema::connection($this->getConnection())->disableForeignKeyConstraints();
 
         Schema::create('diaries', function (Blueprint $table) {
             $table->id();
