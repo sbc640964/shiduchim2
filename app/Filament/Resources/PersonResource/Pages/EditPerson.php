d3446bfe7da2f86d14db3fce39e1c9cb155e98cb
abc640964 <sbc640964@gmail.com>
2025-02-13 14:19:48 +0200

    fix deleted and created phones

--- app/Filament/Resources/PersonResource/Pages/EditPerson.php
+++ app/Filament/Resources/PersonResource/Pages/EditPerson.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\PersonResource\Pages;
 
 use App\Filament\Resources\PersonResource;
+use App\Models\Call;
 use App\Models\Person;
+use App\Models\Phone;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
 use Filament\Forms\Components\DatePicker;
@@ -46,5 +48,29 @@ class EditPerson extends EditRecord
     public function refreshFormDataB(array $attributes): void
     {
         $this->refreshFormData($attributes);
+    }
+
+    public function beforeSave(): void
+    {
+        $sourcePhones = ($this->record->phones?->merge($this->record->family?->phones ?? collect()) ?? collect());
+
+        $updatePhones = collect($this->data['phones'] ?? [])->merge($this->data['family']['phones'] ?? []);
+
+        $deletedPhones = $sourcePhones->pluck('id')->diff($updatePhones->pluck('id'));
+
+        $deletedPhones->isNotEmpty() && Call::query()->whereIn('phone_id', $deletedPhones)->update([
+            'phone_id' => null,
+        ]);
+    }
+
+    public function afterSave(): void
+    {
+        $phones = ($this->record->phones?->merge($this->record->family?->phones ?? collect()) ?? collect());
+
+        $phones->filter(fn (Phone $phone) => $phone->wasRecentlyCreated)->each(function (Phone $phone) {
+            Call::query()->where('phone', $phone->number)->update([
+                'phone_id' => $phone->id,
+            ]);
+        });
     }
 }
