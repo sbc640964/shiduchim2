4487a342bb9417de7347e94cfb7561a145d723c5
abc640964 <sbc640964@gmail.com>
2025-03-06 13:07:07 +0200

    update subscription-info view to display detailed payment and transaction summaries

--- resources/views/filament/resources/student-resource/widgets/subscription-info.blade.php
+++ resources/views/filament/resources/student-resource/widgets/subscription-info.blade.php
@@ -1,4 +1,4 @@
-<x-filament-widgets::widget>
+<x-filament-widgets::widget class="space-y-6">
     <x-filament::section>
         <div class="[&>div]:px-8 flex divide-x divide-x-reverse">
             <div class="flex items-center">
@@ -82,12 +82,61 @@
                             $this->editBilling(),
                             $this->setMatchmaker(),
                             $this->cancelSubscription(),
+                            $this->activities()
                         ]"
                     />
                 </div>
             </div>
         </div>{{-- Widget content --}}
     </x-filament::section>
+
+    <div class="flex gap-6">
+        <x-filament::section class="w-full">
+            <div>
+                <h3 class="text-gray-600">תשלומי הו"ק</h3>
+                <div class="flex gap-2">
+                    <div>
+                        <div class="font-bold text-3xl">
+                            {{Number::currency($this->getSubscription()->transactions->where('is_join')->sum('amount'), in: 'ILS')}}
+                        </div>
+                        <div class="text-sm text-gray-400">
+                            ב {{ $this->getSubscription()->transactions->where('is_join')->count() }} תשלומים, מתוך {{ $this->getSubscription()->payments }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </x-filament::section>
+        <x-filament::section class="w-full">
+            <div>
+                <h3 class="text-gray-600">חודש עבודה נוכחי</h3>
+                <div class="flex gap-2">
+                    <div>
+                        <div class="font-bold text-3xl">
+                            {{ $this->getSubscription()->payments - $this->getSubscription()->balance_payments }}
+                        </div>
+                        <div class="text-sm text-gray-400">
+                            מתוך {{ $this->getSubscription()->payments }} חודשים
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </x-filament::section>
+        <x-filament::section class="w-full">
+            <div>
+                <h3 class="text-gray-600">תשלומים נוספים</h3>
+                <div class="flex gap-2">
+                    <div>
+                        <div class="font-bold text-3xl">
+                            {{Number::currency($this->getSubscription()->transactions->where('is_join', false)->sum('amount'), in: 'ILS')}}
+                        </div>
+                        <div class="text-sm text-gray-400">
+                            ב {{ $this->getSubscription()->transactions->where('is_join', false)->count() }} תשלומים
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </x-filament::section>
+    </div>
 
     <x-filament-actions::modals/>
 </x-filament-widgets::widget>
