dceb6e58eebbb9c158a11489edabf88db8472a43
abc640964 <sbc640964@gmail.com>
2025-03-21 11:02:13 +0200

    Remove unnecessary key binding from active-call-drawer component in calls-box.blade.php

--- resources/views/filament/calls-box.blade.php
+++ resources/views/filament/calls-box.blade.php
@@ -42,7 +42,7 @@
             x-transition:leave-start="opacity-100 translate-x-0"
             x-transition:leave-end="opacity-0 -translate-x-full"
         >
-            <livewire:active-call-drawer :current-call="$this->getCall()" :key="$this->getCall()?->id" />
+            <livewire:active-call-drawer :current-call="$this->getCall()" />
         </div>
 </div>
 
