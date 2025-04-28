4e3b0bd86b8d14dc5c6af6240fca528d5f345ccc
abc640964 <sbc640964@gmail.com>
2025-04-28 11:54:08 +0300

    Add Sentry integration and update dependencies
    Integrated Sentry into the Laravel project for better error tracking and monitoring. Upgraded several dependencies, including Filament packages and others, to their latest versions for improved stability and functionality. Updated composer.lock to reflect the new changes.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,5 +26,5 @@ return Application::configure(basePath: dirname(__DIR__))
         })->everyTwoMinutes();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->create();
