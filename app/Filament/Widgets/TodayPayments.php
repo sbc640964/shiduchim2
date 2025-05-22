27ea7b6ce64c865507b99158f8366d6905b479f2
abc640964 <sbc640964@gmail.com>
2025-05-22 16:04:00 +0300

    Add TodayPayments widget for daily payment overview
    Introduce TodayPayments widget in the Filament dashboard to display daily payment stats. It includes a breakdown of received payments and pending amounts, enhancing financial visibility. Access is restricted based on subscription permissions.

---
+++ app/Filament/Widgets/TodayPayments.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\StudentResource\Pages\Subscription;
+use App\Models\Payment;
+use App\Models\Subscriber;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class TodayPayments extends BaseWidget
+{
+
+    protected static ?int $sort = -200;
+    public static function canView(): bool
+    {
+        return Subscription::canAccess();
+    }
+
+    protected function getStats(): array
+    {
+        $notPay = Subscriber::whereDate('next_payment_date', "<=", today())
+            ->whereIn('status', ['active', 'completed-active'])
+            ->sum('amount');
+
+        $pay = Payment::query()
+            ->whereDate('created_at', today())
+            ->whereStatus('OK')
+            ->sum('amount');
+
+        return [
+            Stat::make('תשלומים היום', \Number::format($pay,2, locale: 'he'))
+                ->color($pay < $notPay ? 'danger' : 'success')
+                ->icon('heroicon-o-currency-dollar')
+                ->description('תשלומים שלא בוצעו היום: ' . \Number::format($notPay, 2, locale: 'he'))
+        ];
+    }
+}
