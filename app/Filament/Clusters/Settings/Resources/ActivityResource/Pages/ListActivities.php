1105f7bb363746e365a53e8ee6ca9bb99461baa6
abc640964 <sbc640964@gmail.com>
2025-07-03 16:51:33 +0300

    Update access control in ActivityResource and remove unused create action in ListActivities

--- app/Filament/Clusters/Settings/Resources/ActivityResource/Pages/ListActivities.php
+++ app/Filament/Clusters/Settings/Resources/ActivityResource/Pages/ListActivities.php
@@ -14,7 +14,7 @@ class ListActivities extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+
         ];
     }
 }
