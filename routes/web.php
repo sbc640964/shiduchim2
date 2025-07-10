bf98e0f664b5f894bc75aef88693210254757323
abc640964 <sbc640964@gmail.com>
2025-07-11 01:48:22 +0300

    Remove test route simulating FPM processing delay

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-Route::get('/test-pm', function () {
-    sleep(10); // מדמה עבודה של FPM
-    return 'ok';
-});
-
 Route::get('webhook/gis', \App\Http\Controllers\WebhookGisController::class);
